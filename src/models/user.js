1cc4ae6d86d0b4c4031202b055359a243a73b07e
Julius Olajumoke <juliusolajumokecoder@gmail.com>
2025-01-17 20:22:04 +0100

    fixed issue with bcrypt on render

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,6 @@
 // src/models/user.js
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
