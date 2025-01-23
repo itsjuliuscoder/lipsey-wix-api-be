1711597373815a43a3793ff03aaee11d68612bd4
Julius Olajumoke <juliusolajumokecoder@gmail.com>
2025-01-23 11:46:58 +0100

    new changes made

--- src/app.js
+++ src/app.js
@@ -10,7 +10,15 @@ const app = express();
 require('dotenv').config();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 app.use(express.json());
 
 app.get('/', (req, res) => {
